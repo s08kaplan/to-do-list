543c3072212029f9acf95872e3eb1fbcaf5a1e18
Serkan <s8ersis@gmail.com>
2023-11-27 07:28:50 +0300

    to learn react to-do-list app started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container bg-success d-flex flex-column justify-content-center text-light">
+        <h1>Welcome ❤</h1>
+        <MyForm />
+      </div>
     </div>
   );
 }
 
+function MyForm() {
+  const [inputValue, setInputValue] = useState("");
+  const [text, setText] = useState("");
+
+  const change = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const formSubmit = (e) => {
+    e.prevent.default();
+    setText(inputValue);
+    setInputValue("");
+  };
+  return (
+    <form onSubmit={formSubmit}>
+      <input
+        style={{ width: "30rem" }}
+        type="text"
+        id="textInput"
+        className="form-control"
+        value={inputValue}
+        onChange={change}
+        placeHolder="🖋📝"
+      />
+    </form>
+  );
+}
+
 export default App;
+
+
+
